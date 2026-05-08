cfe6a84c31891596ca329281174edbe2fccde9d4
kezzzzzqwqw <keziakeilanni@gmail.com>
2026-05-09 01:01:05 +0800

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const today = new Date();
+
+const currentYear = today.getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
